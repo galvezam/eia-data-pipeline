d876f548b3f6929ce4d93e6db4151b1319511210
galvezam <mgalvez644@icloud.com>
2026-05-25 21:41:42 +0000

    added config for dashboard

---
+++ dashboard/config.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+AWS_ACCESS_KEY = st.secrets["AWS_ACCESS_KEY"]
+AWS_SECRET_KEY = st.secrets["AWS_SECRET_KEY"]
+AWS_REGION     = st.secrets["AWS_REGION"]
+BUCKET_NAME    = st.secrets["BUCKET_NAME"]
